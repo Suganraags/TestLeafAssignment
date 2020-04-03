aca3a41fb42067fb6f71afc3a524803bf9a1dd99
Suganraags <62995267+Suganraags@users.noreply.github.com>
2020-04-03 20:41:20 +0530

    Update Assignment1.java

--- Assignment1.java
+++ Assignment1.java
@@ -10,21 +10,34 @@ public class Assignment1 {
 	public static void main(String[] args) throws InterruptedException {
 		System.setProperty("webdriver.chrome.driver","./chromedriver.exe");
 		ChromeDriver driver = new ChromeDriver();
+		//Launch URL:
 		driver.navigate().to("https://acme-test.uipath.com/account/login");
+		// Enter UserName (kumar.testleaf@gmail.com)
 		driver.findElementById("email").sendKeys("kumar.testleaf@gmail.com");
+		// and TAB
 		driver.findElementById("email").sendKeys(Keys.TAB);
+		//enter passowrd
 		driver.findElementById("password").sendKeys("leaf@12");
+		//Click Login
 		driver.findElementById("buttonLogin").click();
 		Thread.sleep(3000);
+		//Mouse over on Vendors
 		WebElement ele = driver.findElementByXPath("//button[text()[normalize-space()='Vendors']]");
 		Actions builder = new Actions(driver);
 		builder.moveToElement(ele).perform();
+		//Click on Search for Vendor
 		driver.findElementByLinkText("Search for Vendor").click();
+		// Enter Vendor name as Blue Lagoon
 		driver.findElementById("vendorName").sendKeys("Blue Lagoon");
+		//Click on search
 		driver.findElementById("buttonSearch").click();
+		//Get the country name
 		String country = driver.findElementByXPath("//td[text()='France']").getText();
+		//print it in console
 		System.out.println(country);
+		//logout
 		driver.findElementByLinkText("Log Out").click();
+		//close the browser
 		driver.close();
 				
 
